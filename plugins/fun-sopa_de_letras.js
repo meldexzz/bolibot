75b05b7622d6fd8f2a4d6892c24da696c0162719
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-03-11 16:47:30 -0600

    Update fun-sopa_de_letras.js

--- plugins/fun-sopa_de_letras.js
+++ plugins/fun-sopa_de_letras.js
@@ -165,5 +165,5 @@ return
 handler.help = ['sopa', 'buscarpalabras']; 
  handler.tags = ['game']; 
 handler.command = /^(buscarpalabra|sopa|soup|wordsearch|wordfind|spdeletras|spletras|sppalabras|spalabras|spdepalabras)$/i
-handler.register = true
+handler.register = false
 export default handler
