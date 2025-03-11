3e3818a9775a57a10f1ad6100044418e1a299792
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-03-11 16:31:50 -0600

    Update descargas-ig.js

--- plugins/descargas-ig.js
+++ plugins/descargas-ig.js
@@ -71,7 +71,7 @@ handler.help = ['instagram *<link ig>*']
 handler.tags = ['downloader']
 handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i
 handler.limit = 1
-handler.register = true 
+handler.register = false 
 export default handler;
 
 const getBuffer = async (url, options) => {
