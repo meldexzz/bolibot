ceb7aca8df8347cc8adcf33e9dc04ead5f7b3615
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-03-12 17:49:54 -0600

    Update descargas-spotify.js

--- plugins/descargas-spotify.js
+++ plugins/descargas-spotify.js
@@ -76,7 +76,6 @@ handler.tags = ['downloader']
 handler.command = /^(spotify|music)$/i
 handler.register = false
 //handler.limit = 1
-handler.level = 1
 export default handler
 
 async function spotifyxv(query) {
