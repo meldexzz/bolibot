5f6563de959cefccb265173923a3cfb6814e7728
DexterZ9 <dexterdp909@gmail.com>
2025-03-12 19:11:24 -0600

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,31 +1,83 @@
 import { WAMessageStubType } from '@whiskeysockets/baileys'
+import fs from 'fs'
+import path from 'path'
 import fetch from 'node-fetch'
 
+
+async function getUserName(conn, jid) {
+  let name = await conn.getName(jid)
+  if (!name) {
+    const contact = await conn.fetchContact(jid)
+    name = contact?.notify || contact?.name || jid.split('@')[0]
+  }
+  return name
+}
+
+function getGroupIcon(m) {
+  const dirPath = path.resolve('./groupIcons')
+  const groupIconPath = path.join(dirPath, `${m.chat}.jpg`)
+
+  if (fs.existsSync(groupIconPath)) {
+    return fs.readFileSync(groupIconPath)
+  }
+  return null
+}
+
+async function getUserProfilePicture(conn, jid) {
+  try {
+    const ppUrl = await conn.profilePictureUrl(jid, 'image')
+    if (ppUrl) {
+      return await (await fetch(ppUrl)).buffer()
+    }
+  } catch (e) {
+    
+  }
+  return null
+}
+
+
+
 export async function before(m, { conn, participants, groupMetadata }) {
-  if (!m.messageStubType || !m.isGroup) return true
+  if (!m.messageStubType || !m.isGroup) return !0
 
   let who = m.messageStubParameters[0]
   let taguser = `@${who.split('@')[0]}`
   let chat = global.db.data.chats[m.chat]
-  let defaultImage = 'https://files.catbox.moe/xr2m6u.jpg';
 
-  if (chat.welcome) {
-    let img;
-    try {
-      let pp = await conn.profilePictureUrl(who, 'image');
-      img = await (await fetch(pp)).buffer();
-    } catch {
-      img = await (await fetch(defaultImage)).buffer();
-    }
+  
+  const userJid = m.messageStubParameters[0]
+  let img = await getUserProfilePicture(conn, userJid)
 
-    if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
-      let bienvenida = `❀ *Bienvenido* a ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom1}\n •(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
-      await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] })
-    } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
-      let bye = `❀ *Adiós* De ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom2}\n •(=^●ω●^=)• Te esperamos pronto!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
-      await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] })
-    }
+  
+  if (!img) {
+    img = getGroupIcon(m)
   }
 
-  return true
-}
+  if (!img) {
+    img = imagen1
+  }
+
+  //const userName = await getUserName(conn, userJid)
+
+  if (chat.welcome) {
+    let message = ''
+    if (m.messageStubType == 27) {
+      message = chat.sWelcome
+        ? chat.sWelcome.replace('@user', taguser).replace('@subject', groupMetadata.subject)
+        : `_🙂 Hola *${taguser}* Bienvenid@ al grupo *${groupMetadata.subject}*_`
+    } else if (m.messageStubType == 32) {
+      message = chat.sBye
+        ? chat.sBye.replace('@user', taguser).replace('@subject', groupMetadata.subject)
+        : `_👋 *${taguser}* Ha abandonado el grupo_`
+    } else if (m.messageStubType == 28) {
+      message = chat.sBye
+        ? chat.sBye.replace('@user', taguser).replace('@subject', groupMetadata.subject)
+        : `_☠️ *${taguser}* Fue expulsad@ del grupo_`
+    }
+
+    if (message) {
+      await conn.sendMessage(m.chat, { image: img, caption: message, mentions: [userJid] })
+    }
+  }
+  }
+    
