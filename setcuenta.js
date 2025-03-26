651c6f25f6182eed5516a689a56cd10b76445544
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-03-26 12:40:57 -0600

    Create setcuenta.js

---
+++ setcuenta.js
@@ -0,0 +1,13 @@
+let handler = async (m, { conn, text, isROwner, isOwner }) => {
+    if (text) {
+        global.db.data.chats[m.chat].setcuenta = text; // Guardamos el set de Cuenta
+        conn.reply(m.chat, '𝙇𝙤𝙨 𝙘𝙤𝙢𝙗𝙤𝙨 𝙙𝙚 𝘾𝙪𝙚𝙣𝙩𝙖 𝙝𝙖𝙣 𝙨𝙞𝙙𝙤 𝙖𝙘𝙩𝙪𝙖𝙡𝙞𝙯𝙖𝙙𝙤𝙨.', m);
+    } else {
+        throw `𝙀𝙨𝙘𝙧𝙞𝙗𝙚 𝙡𝙤𝙨 𝙘𝙤𝙢𝙗𝙤𝙨 𝙦𝙪𝙚 𝙙𝙚𝙨𝙚𝙖𝙨 𝙚𝙨𝙩𝙖𝙗𝙡𝙚𝙘𝙚𝙧 𝙚𝙣 𝙚𝙨𝙩𝙚 𝙜𝙧𝙪𝙥𝙤, 𝙚𝙟𝙚𝙢𝙥𝙡𝙤: \n.setcuenta 𝙇𝙤𝙨 𝙢𝙚𝙟𝙤𝙧𝙚𝙨 𝙡𝙤𝙠𝙤𝙣𝙤𝙨.`;
+    }
+};
+
+handler.command = ['setcuenta']; // Comando de Cuenta
+handler.admin = true;
+handler.group = true;
+export default handler;
